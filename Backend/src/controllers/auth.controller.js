da18aefeae454e38557fbe3fdfbef30dee3be257
akash0528 <akashdivakar852@.com>
2026-07-23 12:44:28 +0530

    updated

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -100,7 +100,12 @@ export async function login(req, res) {
         username: user.username,
     }, process.env.JWT_SECRET, { expiresIn: '7d' })
 
-    res.cookie("token", token)
+   res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "None",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
 
     res.status(200).json({
         message: "Login successful",
@@ -193,7 +198,11 @@ export async function verifyEmail(req, res) {
  * @access Private
  */
 export async function logout(req, res) {
-    res.clearCookie("token");
+    res.clearCookie("token", {
+  httpOnly: true,
+  secure: true,
+  sameSite: "None",
+});
 
     res.status(200).json({
         message: "Logged out successfully",
